623fa8b345870d0769664cde648f3a4715960205
David Pettifer <david.p@dizzy.co.uk>
2016-07-30 23:11:37 +0100

    Made homepage dynamic, integrated MongoDB for products

---
+++ controllers/homepage/index.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://heroku_7jbfrvs8:76hige1vltbholks0vdnmmpdpo@ds031925.mlab.com:31925/heroku_7jbfrvs8';
+
+exports.index = function(req, res) {	
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  findProducts(db, function(results) {
+		res.render('index', { products: results})
+  	db.close();
+  })
+});
+}
+
+var findProducts = function(db, callback) {
+   var results = db.collection('products').find().toArray(function(err, results) {
+		callback(results)
+	 })
+		
+};	
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', require('./../controllers/homepage').index);
 
 router.get('/products', require('./../controllers/products').index)
 
