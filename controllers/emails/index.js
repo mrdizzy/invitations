9f64d9ec8ab154ccf96bf3d2243c243a241d9ca4
mrdizzy <david.pettifer@dizzy.co.uk>
2017-02-18 19:45:58 +0000

    Updated

--- controllers/emails/index.js
+++ controllers/emails/index.js
@@ -2,12 +2,13 @@
 exports.create = function(req, res, next) {
 
 var helper = require('sendgrid').mail;
-var mail = new helper.Mail(from_email, subject, to_email, content);
 
 var from_email = new helper.Email('david@dreamsonpaper.co.uk');
 var to_email = new helper.Email(req.body.email);
 var subject = 'Thank you for ordering your sample box...';
 var content = new helper.Content('text/plain', "Dear " + req.body.name + ",\n\nThank you for requesting a sample pack. It will be sent to you today!");
+
+var mail = new helper.Mail(from_email, subject, to_email, content);
 
    var sg = require('sendgrid')("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");
 var request = sg.emptyRequest({
@@ -20,8 +21,8 @@ sg.API(request, function(error, response) {
   console.log(response.statusCode);
   console.log(response.body);
   console.log(response.headers);
-  unless(error) {
-       res.send(200)
+  if(!error) {
+       res.sendStatus(200)
   }
 });
 };
