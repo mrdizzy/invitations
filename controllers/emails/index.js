890f8cecdb758e3135be13081e1bd7f04a0972cf
mrdizzy <david.pettifer@dizzy.co.uk>
2017-02-19 18:54:17 +0000

    Updated

--- controllers/emails/index.js
+++ controllers/emails/index.js
@@ -1,16 +1,17 @@
-
 exports.create = function(req, res, next) {
 
 var helper = require('sendgrid').mail;
-
 var from_email = new helper.Email('david@dreamsonpaper.co.uk');
 var to_email = new helper.Email(req.body.email);
 var subject = 'Thank you for ordering your sample box...';
 var content = new helper.Content('text/plain', "Dear " + req.body.name + ",\n\nThank you for requesting a sample pack. It will be sent to you today!");
 
+
+console.log(from_email, subject, to_email, content)
+
 var mail = new helper.Mail(from_email, subject, to_email, content);
 
-   var sg = require('sendgrid')("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");
+   var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',
