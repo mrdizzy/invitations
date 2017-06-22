06457dbb0891e9bb8a91bbab3eca3f59b8c8ad9f
mrdizzy <david.pettifer@dizzy.co.uk>
2017-06-22 15:35:12 +0000

    Updated

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -4,214 +4,62 @@ $(function() {
         urlRoot: '/products',
         idAttribute: "_id",
         defaults: {
-            "embellishment": false,
-            "format": "flat",
-            "texture": "plain",
-            "envelopestyle": "plain",
+            "card_format": "A6_Flat",
+            "card_texture": "Plain_lightweight",
+            "envelope_style": "Plain",
             "personalisation": [],
             "prices": {},
             "extras": [],
-            "guestaddressing": [],
+            "guest_personalisation": [],
             "quantity": 20,
             "price": 1.49
         },
         initialize: function() {
-            this.on("change", this.calculateCost)
+            this.on("change", this.calculatePrice)
         },
-        calculateCost: function() {
-            var quantity = this.get("quantity"),
-                texture = this.get("texture"),
-                embellishment = this.get("embellishment"),
-                format = this.get("format"),
-                envelopestyle = this.get("envelopestyle"),
-                extras = this.get("extras"),
-                total;
-
-            var cost_of_tissue_paper = CazCalculate([
-                ["tissue_paper", 0.25]
-            ])
-
-            var invitation_size = 0.25
-            
-            if (format == "foldout") {
-                invitation_size = 0.5
-            }
-
-                var invitation_card = "plain_400"
-            if (texture != "plain") {
-                invitation_card = texture + "_" + 300;
-            }
-            
-            var cost_per_invitation = CazCalculate([
-                    [invitation_card, invitation_size]
-                ])
-            if (format == "foldout") {
-                var cost_per_invitation = CazCalculate([
-                    [invitation_card, invitation_size],
-                    ["vellum", 0.5]
-                ])
-            }
-          
-
-            // Envelope
-            if ((envelopestyle == "plain") || (envelopestyle == "matching")) {
-                var envelope_texture = texture + "_" + "paper"
-            }
-            else if ((envelopestyle == "boxplain") || (envelopestyle == "boxmatching")) {
-                if (texture == "plain") {
-                    var envelope_texture = "plain_400"
-                }
-                else {
-                    var envelope_texture = texture + "_" + "300"
-                }
-            }
-            var cost_per_envelope = CazCalculate([
-                [envelope_texture, 1]
-            ])
-
-
-
-            // Embellishments
-            if (embellishment) {
-                if (embellishment == "wrap") {
-                    var wrap_texture = texture + "_" + "paper";
-                    var cost_per_embellishment = CazCalculate([
-                        [wrap_texture, 0.20]
-                    ]);
-
-                }
-                else {
-
-                    var cost_per_embellishment = CazCalculate([
-                        [embellishment, 1]
-                    ])
-
-                }
+        calculatePrice: function() {
+            var prices = {};
+          var options = Object.keys(caz_options)
+          var price = this.get("price")
+          var that = this;
+          var direction_map=0;
+          options.forEach(function(option) {
+                   var choice = that.get(option);
+            if((option == "guest_personalisation") || (option == "extras")) {
+              if(choice) {
+                choice.forEach(function(achoice) {
+                  if (achoice == "Direction_map") {
+                    direction_map = 25
+                  } else {
+                  cost = caz_options[option][achoice]["cost"]
+                  price= price + cost
+                  }
+                })
+              }
             }
             else {
-                var cost_per_embellishment = 0;
+                   if(choice) {
+                     var cost = caz_options[option][choice]["cost"];
+                     price = price + cost;
+                   }
             }
-            var cost_of_extras = 0;
-            extras.forEach(function(extra) {
-                if (extra == "rsvps") {
-                    cost_of_extras = cost_of_extras + CazCalculate([
-                        [invitation_card, 0.125]
-                    ]) + cost_of_tissue_paper;
-                }
+          })
+          
+           var quantities = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170];
 
-                if (extra == "info") {
-                    cost_of_extras = cost_of_extras + CazCalculate([
-                        [invitation_card, 0.25]
-                    ]) + cost_of_tissue_paper
-                }
-            })
-            var total_cost_each = cost_per_envelope + cost_per_invitation + cost_per_embellishment + cost_of_extras;
-        
-            var price = this.get("price"),
-                guestaddressing = this.get("guestaddressing"),
-                total;
-
-            var quantities = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170];
-
-            var prices = {};
-            if (format == "foldout") {
-                price = price + 0.59
-            }
-
-            //ENVELOPE STYLE
-            if (envelopestyle == "boxplain") {
-                price = price + 0.67
-            }
-            else if (envelopestyle == "matching") {
-                price = price + 0.89
-            }
-            else if (envelopestyle == "boxmatching") {
-                price = price + 1.36
-            }
-            var map;
-
-            // CARD TEXTURE
-            if (texture == "linen") {
-                price = price + 0.24
-            }
-            else if (texture == "hammered") {
-                price = price + 0.24
-            }
-            else if (texture == "pearlescent") {
-                price = price + 0.44
-            }
-            else if (texture == "kraft") {
-                price = price + 0.44
-            }
-            else if (texture == "laid") {
-                price = price + 0.24
-            }
-
-            // EMBELLISHMENT
-            if (embellishment == "ribbon") {
-                price = price + 0.32
-            }
-            else if (embellishment == "pearl") {
-                price = price + 1.79
-            }
-            else if (embellishment == "wrap") {
-                price = price + 0.17
-            }
-            else if (embellishment == "acrylic-heart") {
-                price = price + 0.57
-            } else if (embellishment == "acrylic-rectangle") {
-                
-                price = price + 0.49
-            } else if (embellishment == "diamante-heart") {
-                price = price + 1.29
-            } else if (embellishment == "diamante-circle") {
-                price = price + 1.47
-            } else if (embellishment == "diamante-square") {
-                price = price + 1.47
-            }
-            
-            extras.forEach(function(extra) {
-                if (extra == "rsvps") {
-                    price = price + 0.37;
-                }
-                if (extra == "info") {
-                    price = price + 0.49;
-                }
-                if (extra == "map") {
-                    map = true;
-                }
-                if (extra == "rounded") {
-                    price = price + 0.25
-                }
-
-            })
-
-            guestaddressing.forEach(function(guest) {
-                if (guest == "addressesprinted") {
-                    price = price + 0.18;
-                }
-                if (guest == "namesprinted") {
-                    price = price + 0.16;
-                }
-
-            })
             var discounts = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
             for (var i = 0, len = quantities.length; i < len; i++) {
 
                 var qty = quantities[i]
                 var total = (price * qty)
-                if (map) {
-                    total = total + 30
-                }
-                total = total - ((discounts[i] / 100) * total)
+              // if (map) {
+              //     total = total + 30
+              // }
+                total = total - ((discounts[i] / 100) * total) + direction_map
                 total = total.toFixed(2);
-                //split_total = total.toString().split(".")
-
-                //  this.set("pounds", split_total[0]).set("pence", split_total[1]).set("total", total)
                 prices[qty] = total;
             }
             this.set("prices", prices)
-
         },
         makePurchase: function() {
             var discount = this.get("discount");
@@ -234,11 +82,11 @@ $(function() {
         el: '#steps',
         initialize: function() {
             this.listenTo(this.model, "change:prices", this.renderPrices)
-            this.model.calculateCost();
+            this.model.calculatePrice();
             this.renderPrices();
         },
         events: {
-            "click .choice": "chosen",
+            "click .radio_choice": "chosen",
             "click .checkbox_choice": "checkboxChosen",
             "click .quantity_box": "chooseQuantity",
         },
@@ -262,7 +110,7 @@ $(function() {
         chosen: function(e) {
             var $element = $(e.currentTarget);
             var id = $(e.currentTarget).attr('id')
-            var ary = id.split("_");
+            var ary = id.split("-");
             var option = ary[0];
             var choice = ary[1];
             $('.' + 'choice_' + option).removeClass("selected_choice")
@@ -274,19 +122,21 @@ $(function() {
             this.model.set(option, choice)
         },
         checkboxChosen: function(e) {
+            if(!$(e.currentTarget).attr("disabled")) {
             var $element = $(e.currentTarget);
             var id = $(e.currentTarget).attr('id')
-            var box = id.split("_")[0]
-            var group = id.split("_")[1]
-            var checkBox = $("input[value='" + box + "']");
+            var option = id.split("-")[0]
+            var choice = id.split("-")[1]
+            var checkBox = $("input[value='" + choice + "']");
 
             $element.toggleClass("selected_choice")
             checkBox.prop("checked", !checkBox.prop("checked"));
             var names = []
-            $("input[name='" + group + "']:checked").each(function() {
+            $("input[name='" + option + "']:checked").each(function() {
                 names.push($(this).val());
             });
-            this.model.set(group, names).trigger("change")
+            this.model.set(option, names).trigger("change")
+            }
         },
         render: function() {}
     })
