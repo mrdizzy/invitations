78016c8cdc7e838071d870041b43bcef49b5dd73
mrdizzy <david.pettifer@dizzy.co.uk>
2017-10-02 14:07:13 +0000

    Updated

--- controllers/homepage/index.js
+++ controllers/homepage/index.js
@@ -4,33 +4,32 @@ var counter = 0;
 var url = 'mongodb://heroku_7jbfrvs8:76hige1vltbholks0vdnmmpdpo@ds031925.mlab.com:31925/heroku_7jbfrvs8';
 
 exports.index = function(req, res) {
- var cookie = req.cookies.sample_request;
+    var cookie = req.cookies.sample_request;
 
-  if ((cookie === undefined) && req.query.sample)
-  {
-    // no: set a new cookie
-    res.cookie('sample_request', req.query.sample/100, { maxAge: 900000 });
-    cookie = req.query.sample/100
-  }
-MongoClient.connect(url, function(err, db) {
+    if ((cookie === undefined) && req.query.sample) {
+        // no: set a new cookie
+        res.cookie('sample_request', req.query.sample / 100, { maxAge: 900000 });
+        cookie = req.query.sample / 100
+    }
+    MongoClient.connect(url, function(err, db) {
 
-  assert.equal(null, err);
-  findProducts(db, function(results) {
-   // if(counter == 0) {
-	res.render('index_a', { products: results, page: "index_a", sample: cookie})
-	//	counter = 1;
-    //  } else {
-//	res.render('index_b', { products: results, page: "index_b", sample:cookie})
-//		counter = 0;
-   //   }
-  	db.close();
-  })
-});
+        assert.equal(null, err);
+        findProducts(db, function(results) {
+            // if(counter == 0) {
+            res.render('index_a', { products: results, page: "index_a", sample: cookie })
+            //	counter = 1;
+            //  } else {
+            //	res.render('index_b', { products: results, page: "index_b", sample:cookie})
+            //		counter = 0;
+            //   }
+            db.close();
+        })
+    });
 }
 
 var findProducts = function(db, callback) {
-   var results = db.collection('products').find().sort({"position": -1}).toArray(function(err, results) {
-		callback(results)
-	 })
+    var results = db.collection('products').find().sort({ "position": -1 }).toArray(function(err, results) {
+        callback(results)
+    })
 
 };
