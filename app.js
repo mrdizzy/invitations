76b9acb9189711ff6bac700af934bc1390c1097d
mrdizzy <david.pettifer@dizzy.co.uk>
2017-10-30 11:00:16 +0000

    Updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 
 var emailLog = function(req, res, next) {
-      if (!(/Pingdom/.test("Pigdom.com_bot_version_1.4"))) {
+      if (!(/Pingdom/.test(req.get("Referer")))) {
       var helper = require('sendgrid').mail;
         var from_email = new helper.Email('david@dizzy.co.uk');
         var to_email = new helper.Email('david.pettifer@dizzy.co.uk');
